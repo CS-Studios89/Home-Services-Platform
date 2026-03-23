d2c7b92755f90c7a29c33926ebe22e3042736e0d
CS-Studios <charbel.sayah@adontechnologies.com>
2026-03-23 23:52:40 +0200

    update profile info api

--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { getProfileInfo } = require('../controllers/profileController');
+const { getProfileInfo, updateProfileInfo } = require('../controllers/profileController');
 const authenticate = require('../middleware/authMiddleware');
 
 router.get('/', authenticate, getProfileInfo);
+router.patch('/', authenticate, updateProfileInfo);
 
 module.exports = router;
