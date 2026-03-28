b350356a2ad7b95d42c4bdc49bfe4632c5c40bfb
CS-Studios <charbel.sayah@adontechnologies.com>
2026-03-28 13:08:15 +0200

    Add Cart Item API

--- backend/controllers/cartsController.js
+++ backend/controllers/cartsController.js
@@ -24,7 +24,7 @@ exports.getCartItems = async (req, res, next) => {
         , [user_id]);
 
         if(!cartItemsResult || !cartItemsResult.rows || !cartItemsResult.rows.length){
-            const cart = await db.query(`Select * From carts Where id = $1 and status = 'active'`, [user_id]);
+            const cart = await db.query(`Select * From carts Where user_id = $1 and status = 'active'`, [user_id]);
             if(!cart || !cart.rows || !cart.rows.length){
                 db.query(`Insert Into carts(user_id, status) values($1, 'active')`);
             }
@@ -37,34 +37,88 @@ exports.getCartItems = async (req, res, next) => {
     }
 }
 
-// exports.addCartItem = async (req, res, next) => {
-//     try {
-
-//         const authHeader = req.headers['authorization'];
+exports.addCartItem = async (req, res, next) => {
+    let client;
+    let inTransaction = false;
+    try {
+        client = await db.connect();
+        const authHeader = req.headers['authorization'];
         
-//         if (!authHeader || !authHeader.startsWith('Bearer '))
-//             return res.status(401).json({ error: 'No token provided' });
+        if (!authHeader || !authHeader.startsWith('Bearer '))
+            return res.status(401).json({ error: 'No token provided' });
 
-//         const token = authHeader.split(' ')[1];
+        const token = authHeader.split(' ')[1];
 
-//         // Verify JWT
-//         const payload = jwt.verify(token, process.env.JWT_SECRET);
-//         // req.user = payload; // attach user_id
-//         const { user_id } = payload;
+        // Verify JWT
+        const payload = jwt.verify(token, process.env.JWT_SECRET);
+        // req.user = payload; // attach user_id
+        const { user_id } = payload;
 
-//         const { cartItem } = req.body;
+        const { cartItem } = req.body;
 
-//         if(!cartItem || !cartItem.offeringId || !cartItem.start_at || !cartItem.end_at || !cartItem.hours){
-//                 return res.status(401).json({message : "Please fill all required fields"});
-//         }
+        if(!cartItem || !cartItem.offeringId || !cartItem.start_at || !cartItem.end_at || !cartItem.hours){
+                return res.status(401).json({message : "Please fill all required fields"});
+        }
 
-        
+        await client.query('BEGIN');
+        inTransaction = true;
 
-//         return res.json({success : true, offerId : resultt});
-//     } catch (err) {
-//         next(err);
-//     }
-// }
+        let cartId;
+        const cart = await client.query(`Select * From carts Where user_id = $1 and status = 'active'`, [user_id]);
+        if(!cart || !cart.rows || !cart.rows.length){
+            const newCart = await client.query(`Insert Into carts(user_id, status) values($1, 'active') Returning id`, [user_id]);
+            cartId = newCart.rows[0].id;
+        }
+        else{
+            cartId = cart.rows[0].id;
+        }
+
+        const busyTimes = await client.query(
+            `Select t.start_at, t.end_at
+            From time_slots t, offerings o
+            Where o.id = $1 and t.provider_id = o.provider_id`
+        );
+
+        let validTime = true;
+        if(busyTimes && busyTimes.rows && busyTimes.rows.length){
+            for(let i = 0; i < busyTimes.rows.length; i++){
+                if(cartItem.start_at > new Date(busyTimes.rows[i].start_at).getTime() 
+                    && cartItem.start_at < new Date(busyTimes.rows[i].end_at).getTime() 
+                    ||
+                    cartItem.end_at > new Date(busyTimes.rows[i].start_at).getTime()
+                    && cartItem.end_at < new Date(busyTimes.rows[i].end_at).getTime()){
+                        validTime = false;
+                        break;
+                    }
+            }
+        }
+
+        if(!validTime){
+            await client.query(`ROLLBACK`);
+            inTransaction = false;
+            return res.status(400).json({message:"Provider is busy during the selected time"});
+        }
+
+        await client.query(
+            `Insert Into cart_items(cart_id, offering_id, start_at, end_at, hours)
+                values($1, $2, $3, $4, $5)`
+        , [cartId, cartItem.offeringId, new Date(cartItem.start_at), new Date(cartItem.end_at), cartItem.hours]);
+
+        return res.json({success : true, offerId : resultt});
+    } catch (err) {
+        if (inTransaction) {
+            try { await client.query('ROLLBACK'); } catch (_) { }
+        }
+        next(err);
+    }
+    finally {
+        // ALWAYS release back to pool
+        try{
+            if(client) client.release();
+        }
+        catch(err){}
+    }
+}
 
 // exports.editProviderOffer = async (req, res, next) => {
 //     try{
