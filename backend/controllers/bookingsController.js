f2a7065e4cc47ade6b783f1d95aae86baf00d8be
CS-Studios <charbel.sayah@adontechnologies.com>
2026-04-08 20:21:30 +0300

    get Booking Requests

--- backend/controllers/bookingsController.js
+++ backend/controllers/bookingsController.js
@@ -89,3 +89,40 @@ exports.cancelBooking = async (req, res, next) => {
         catch(err){}
     }
 }
+
+exports.getBookingRequests = async (req, res, next) => {
+    try{
+        const authHeader = req.headers['authorization'];
+
+        if (!authHeader || !authHeader.startsWith('Bearer '))
+            return res.status(401).json({ error: 'No token provided' });
+
+        const token = authHeader.split(' ')[1];
+
+        // Verify JWT
+        const payload = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = payload; // attach user_id
+        const { user_id } = payload;
+
+        const isProvider = userModel.isAProvider(user_id);
+        if(!isProvider){
+            return res.status(403).json({message:"You are not a provider"});
+        }
+
+        const bookingResult = await db.query(
+            `Select b.id booking_id, b.status booking_status, oi.start_at, oi.end_at, 
+                oi.price, oi.hours, oi.total, or.curr, o.title, s.name service_name, u.name client_name, 
+                a.country, a.city, a.street, a.building, a.floor, a.apartment
+            From bookings b, order_items oi, orders or, offerings o, 
+                services s, providers p, users u, address a
+            Where p.id = $1 and oi.id = b.order_item_id and or.id = oi.order_id
+                and o.id = oi.offering_id and s.id = o.service_id and p.id = o.provider_id
+                and u.id = b.user_id and a.id = b.addr_id and b.status = $2`
+        , [user_id, "requested"]);
+
+        return res.json(bookingResult.rows);
+    }
+    catch(err){
+        next(err);
+    }
+}
--- backend/routes/booking.js
+++ backend/routes/booking.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getBookings, cancelBooking } = require('../controllers/bookingsController');
+const { getBookings, cancelBooking, getBookingRequests } = require('../controllers/bookingsController');
 const authenticate = require('../middleware/authMiddleware');
 
 router.get('/', authenticate, getBookings); // for user
 router.delete('/:bookingId', authenticate, cancelBooking);
-// router.get('/pending', authenticate, getBookingRequests); // provider receive booking request ( get all booking requests)
+router.get('/pending', authenticate, getBookingRequests); // provider receive booking request ( get all booking requests)
 // router.get('/:bookingId/accept', authenticate, acceptBooking); // provider accept booking
 // router.delete('/:bookingId/reject', authenticate, rejectBooking); // provider reject booking
 
