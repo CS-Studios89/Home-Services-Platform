dd94b86c5086e0fc84c1f8eb36ba53726faba432
CS-Studios <charbel.sayah@adontechnologies.com>
2026-04-08 19:30:30 +0300

    get Bookings

--- backend/controllers/bookingsController.js
+++ backend/controllers/bookingsController.js
@@ -16,9 +16,17 @@ exports.getBookings = async (req, res, next) => {
         const { user_id } = payload;
 
         const bookingResult = await db.query(
-            `Select 
-                From bookings b, order_items oi, offerings o, services s, providers p, users u, address a`
-        )
+            `Select b.id booking_id, b.status booking_status, oi.start_at, oi.end_at, 
+                oi.price, oi.hours, oi.total, or.curr, o.title, s.name service_name, u.name provider_name, 
+                a.country, a.city, a.street, a.building, a.floor, a.apartment
+            From bookings b, order_items oi, orders or, offerings o, 
+                services s, providers p, users u, address a
+            Where b.user_id = $1 and oi.id = b.order_item_id and or.id = oi.order_id
+                and o.id = oi.offering_id and s.id = o.service_id and p.id = o.provider_id
+                and u.id = p.user_id and a.id = b.addr_id`
+        , [user_id]);
+
+        return res.json(bookingResult.rows);
     }
     catch(err){
         next(err);
